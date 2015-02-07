46e148e3d567f07f723be9a7e1f498e4fced5c6b
Jarek Dziedzic <jarek.dziedzic@gmail.com>
2015-02-07 14:59:21 +0000

    Implemented basic addError() which prints filename of first frame.

--- traceback_hl.py
+++ traceback_hl.py
@@ -1,12 +1,19 @@
 from nose.plugins import Plugin
-
+import sys
 
 class Colorize(Plugin):
     name = 'color'
     enabled = True
 
     def addError(self, test, err, capt):
-        self.stream.write("colorized error: ")
+        self.stream.write("colorized traceback (eventually): ")
+        traceback = sys.exc_info()[2]
+        # TODO: print only if it's the current file (test.py)
+        frame = traceback.tb_frame
+        filename = frame.f_code.co_filename
+        self.stream.write("filename = ")
+        self.stream.write(filename)
+
 
     def setOutputStream(self, stream):
         # grab for own use
