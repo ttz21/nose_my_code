c5076179ecc2fa0dbf47ca71c6d352018778e4e7
greg loyse <gloyse@google.com>
2015-02-07 12:02:15 +0000

    plugin start (not working)

--- test.py
+++ test.py
@@ -2,7 +2,6 @@
 run with:
     nosetests test.py
 
-
 """
 import unittest
 import collections
@@ -19,14 +18,18 @@ class MyTest(unittest.TestCase):
     def test_basic(self):
         try:
             calc()
-        except Exception as exc:
+        except Exception as __:
             traceback = sys.exc_traceback
             frame = traceback.tb_frame
+            import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
             self.assertTrue(hasattr(frame, 'f_lineno'))
             self.assertTrue(hasattr(frame, 'f_code'))
             code = frame.f_code
             self.assertTrue(hasattr(code, 'co_filename'))
 
 
+from traceback_hl import HelloWorld
+
 if __name__ == '__main__':
-    nose.main()
+    import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
+    nose.main(addplugins=[HelloWorld()])
---
+++ traceback_hl.py
@@ -0,0 +1,26 @@
+import logging
+import os
+
+from nose.plugins import Plugin
+
+#log = logging.getLogger('nose.plugins.helloworld')
+
+
+class HelloWorld(Plugin):
+    name = 'helloworld'
+
+    def __init__(self):
+        raise(Exception())
+
+    def options(self, parser, env=os.environ):
+        parser.add_options('--hi', action='store_true')
+        Plugin.options(self, parser, env=env)
+
+
+    def configure(self, options, conf):
+        super(HelloWorld, self).configure(options, conf)
+        if not self.enabled:
+            return
+
+    def finalize(self, result):
+        raise(Exception())
