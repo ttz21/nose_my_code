f641c78c0b2c4ccf1481fa3ca21b45785b560a35
greg loyse <gloyse@google.com>
2015-02-22 14:39:34 +0000

    decouple highlighting from Stream class
        Its easier to test strings than streams and I think the stream should be
        as dumb as possible.
        Removing the toggle state from the class also should make this threadsafe?

--- mycode/__init__.py
+++ mycode/__init__.py
@@ -1,46 +1,47 @@
 from nose.plugins import Plugin
 import os
 
-bold = '\033[1m{0}\033[0m'
-red = "\033[1;31m{0}\033[00m"
-green = "\033[1;32m{0}\033[00m"
-blue = "\033[1;34m{0}\033[00m"
+_bold = lambda a_str: '\x1b[1m{0}\x1b[0m'.format(a_str)
+_red =  lambda a_str: "\x1b[1;31m{0}\x1b[0m".format(a_str)
+_blue = lambda a_str: "\x1b[1;34m{0}\x1b[0m".format(a_str)
+
+
+def highlight_traceback(traceback, cwd):
+    lines = traceback.split('\n')
+
+    # embolden traceback line
+    intro_idx = 0
+    lines[intro_idx] = _bold(lines[intro_idx])
+
+    # higlight mycode
+    skip_next = False
+    for i in range(1, len(lines[:-2])):
+        if skip_next:
+            skip_next = False
+            continue
+        curr, next_ = i, i + 1  # assumption: err lines always pair
+        line = lines[curr]
+        if cwd in line:
+            lines[curr] = _blue(lines[curr])
+            lines[next_] = _blue(lines[next_])
+            skip_next = True
+
+    # red error mesage
+    err_idx = -2
+    lines[err_idx] = _red(lines[err_idx])
+
+    return '\n'.join(lines)
 
 
 class HighlightedStream(object):
 
     def __init__(self, stream):
         self.stream = stream
-        self.current_path = os.getcwd()
-
-    def highlight_traceback_msg(self, msg):
-        lines = msg.split('\n')
-
-        # assumptions: A unit covers 2 lines so total always odd
-        # 2 lines for each unit + 1 line for Traceback string
-        assert len(lines) % 2 == 1
-
-        # embolden traceback line
-        intro_idx = 0
-        lines[intro_idx] = bold.format(lines[intro_idx])
-
-        # higlight mycode
-        for i in range(1, len(lines[-2:]), 2):
-            curr, next_ = i, i + 1
-            line = lines[curr]
-            if self.current_path in line:
-                lines[curr] = blue.format(lines[curr])
-                lines[next_] = blue.format(lines[next_])
-
-        # red error mesage
-        err_idx = -2
-        lines[err_idx] = red.format(lines[err_idx])
-
-        return '\n'.join(lines)
+        self.cwd = os.getcwd()
 
     def write(self, msg):
         if msg.startswith('Traceback'):
-            msg = self.highlight_traceback_msg(msg)
+            msg = highlight_traceback(msg, self.cwd)
             self.stream.write(msg)
         else:
             self.stream.write(msg)
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from mycode import MyCode
 
 from collections import OrderedDict
 
+from mycode import highlight_traceback, _red, _blue, _bold
 
 def ouch():
     OrderedDict('ouch')
@@ -12,9 +13,47 @@ def ouch():
 
 class MyCodeTest(unittest.TestCase):
 
+    @unittest.skip('for manual testing')
     def test_output_is_highlighted(self):
         ouch()
 
+    def test_highlight_traceback(self):
+
+        cwd = '/Users/User/mycode'
+        got = highlight_traceback(a_traceback, cwd)
+
+        red_error = _red('ValueError: need more than 1 value to unpack')
+        self.assertIn(red_error, got)
+
+        blue_lines = blues.split('\n')
+        for line in blue_lines:
+            blue_line = _blue(line)
+            self.assertIn(blue_line, got)
+
+        bold_intro = _bold('Traceback (most recent call last):')
+        self.assertIn(bold_intro, got)
+
+
+blues = \
+"""\
+  File "/Users/User/mycode/test.py", line 20, in test_output_is_highlighted
+    ouch()
+  File "/Users/User/mycode/test.py", line 13, in ouch
+    OrderedDict('ouch')"""
+
+
+a_traceback = \
+"""Traceback (most recent call last):
+  File "/Users/User/mycode/test.py", line 20, in test_output_is_highlighted
+    ouch()
+  File "/Users/User/mycode/test.py", line 13, in ouch
+    OrderedDict('ouch')
+  File "/Users/User/.virtualenvs/dj-light/lib/python3.4/collections/__init__.py", line 56, in __init__
+    self.__update(*args, **kwds)
+  File "/Users/User/.virtualenvs/dj-light/bin/../lib/python3.4/_collections_abc.py", line 602, in update
+    for key, value in other:
+ValueError: need more than 1 value to unpack
+"""
 
 if __name__ == '__main__':
     nose.main(addplugins=[MyCode()])
